c4070bf491a2fc56cd9a0707862a9e30247fbd43
David Duluc <29899921+davidduluc@users.noreply.github.com>
2023-05-07 19:44:22 -0400

    Added react-router, prettier and react-helmet.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import BlogPost from './components/BlogPost';
-import NotFound from './components/NotFound';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import BlogPost from "./components/BlogPost";
+import NotFound from "./components/NotFound";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/blog/:id' element={<BlogPost />} />
-				<Route path='*' element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
